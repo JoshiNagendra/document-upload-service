34f7a19184fc23e57864766cec10dc381b74811a
JoshiNagendra <joshichennamsetti999@gmail.com>
2025-06-14 23:19:22 +0530

    Initial commit for document upload service

---
+++ src/main/java/com/joshi/document/document_upload_service/DocumentUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joshi.document.document_upload_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentUploadServiceApplication.class, args);
+	}
+
+}
